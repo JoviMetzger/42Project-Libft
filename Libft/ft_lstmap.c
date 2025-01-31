7fa03fc2c82481befd7b72f660a82128abf62962
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2025-01-31 15:36:10 +0100

    Update ft_lstmap.c

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -28,14 +28,14 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	new_lst = NULL;
 	while (lst)
 	{
-		new_node = ft_lstnew(f(lst -> content));
+		new_node = ft_lstnew(f(lst->content));
 		if (new_node == 0)
 		{
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
 		ft_lstadd_back(&new_lst, new_node);
-		lst = lst -> next;
+		lst = lst->next;
 	}
 	return (new_lst);
 }
