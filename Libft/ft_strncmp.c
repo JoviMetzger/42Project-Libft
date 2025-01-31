f34ff88d9c20e0262e779c38c33847e44f63f8e5
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2025-01-31 15:45:54 +0100

    Update ft_strncmp.c

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -42,3 +42,29 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n)
 	printf("%d", re);
 	return (0);
 }*/
+
+
+// ----------------------------------------------------------------------
+// ------------------ MORE OPTIONS --------------------------------------
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	if (!s1 && !s2)
+		return (0);
+	if (!s1 && s2)
+		return (-1);
+	if (s1 && !s2)
+		return (1);
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else if (s1[i] > s2[i])
+			return (1);
+		else
+			return (-1);
+	}
+	return (0);
+}
