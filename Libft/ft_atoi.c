bc5059d18de225fa8cd5a800c0b62a27bdb8f49f
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2025-02-12 13:43:19 +0100

    Update ft_atoi.c

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -49,3 +49,45 @@ int	ft_atoi(const char *str)
 	int re = ft_atoi(str);
 	printf("%d", re);
 }*/
+
+
+// ------------------------------------------------------------------------
+// ft_atoi_base()
+int input_check(char c) 
+{   
+    if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F'))
+        return (0);
+    else 
+        return (1);
+}
+
+int ft_atoi_base(const char *str, int base) 
+{
+    int i;
+    int result;
+    int sign;
+    int digit;
+
+    i = 0;
+    result = 0;
+    sign = 1;
+    digit = 0;
+    if (str[i] == '-' || str[i] == '+') 
+    {
+        if (str[i] == '-')
+            sign = -1;
+        i++;
+    }
+    while (str[i] != '\0' && input_check(str[i]) == 0)
+    {
+        if (str[i] >= 'a' && str[i] <= 'f')
+            digit = str[i] - 'a' + 10;
+        else if (str[i] >= 'A' && str[i] <= 'F')
+            digit = str[i] - 'A' + 10;
+        else if (str[i] >= '0' && str[i] <= '9')
+            digit = str[i] - '0';
+        result = result * base + digit;
+        i++;
+    }
+    return (result * sign);
+}
