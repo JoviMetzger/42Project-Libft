40b1e2e2af6398e9da0c733cea7b3a48f42c43d8
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2025-02-16 21:04:45 +0100

    Update libft.h

--- includes/libft.h
+++ includes/libft.h
@@ -21,31 +21,31 @@
 # include <stddef.h>
 
 // mem-functions
-void			*ft_calloc(size_t count, size_t n);
-void			*ft_memchr(const void *str, int ch, size_t n);
-void			*ft_memcpy(void *dest, const void *src, size_t n);
-void			*ft_memmove(void *dest, const void *src, size_t n);
-void			*ft_memset(void *str, int ch, size_t n);
-int				ft_memcmp(const void *str1, const void *str2, size_t n);
+void      *ft_calloc(size_t count, size_t n);
+void      *ft_memchr(const void *str, int ch, size_t n);
+void      *ft_memcpy(void *dest, const void *src, size_t n);
+void      *ft_memmove(void *dest, const void *src, size_t n);
+void      *ft_memset(void *str, int ch, size_t n);
+int       ft_memcmp(const void *str1, const void *str2, size_t n);
 
 // str-functions
-char			*ft_strchr(const char *str, int n);
-char			*ft_strdup(const char *str);
-char			*ft_strnstr(const char *str, const char *ch, size_t n);
-char			*ft_strrchr(const char *str, int n);
-char			*ft_strcpy(char *s1, char *s2);
-char			*ft_strncpy(char *dest, char *src, unsigned int n);
-char			*ft_strcat(char *dest, char *src);
-char			*ft_strncat(char *dest, char *src, unsigned int nb);
-char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-char			*ft_strjoin(char const *s1, char const *s2);
-char			*ft_strtrim(char const *s1, char const *set);
-int				ft_strncmp(const char *str1, const char *str2, size_t n);
-int				ft_strcmp(char *s1, char *s2);
-void			ft_striteri(char *s, void (*f)(unsigned int, char*));
-size_t		ft_strlen(char const *str);
-size_t		ft_strlcpy(char *dest, char const *src, size_t n);
-size_t		ft_strlcat(char *dest, const char *src, size_t n);
+char      *ft_strchr(const char *str, int n);
+char      *ft_strdup(const char *str);
+char      *ft_strnstr(const char *str, const char *ch, size_t n);
+char      *ft_strrchr(const char *str, int n);
+char      *ft_strcpy(char *s1, char *s2);
+char      *ft_strncpy(char *dest, char *src, unsigned int n);
+char      *ft_strcat(char *dest, char *src);
+char      *ft_strncat(char *dest, char *src, unsigned int nb);
+char      *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char      *ft_strjoin(char const *s1, char const *s2);
+char      *ft_strtrim(char const *s1, char const *set);
+int       ft_strncmp(const char *str1, const char *str2, size_t n);
+int       ft_strcmp(char *s1, char *s2);
+void      ft_striteri(char *s, void (*f)(unsigned int, char*));
+size_t    ft_strlen(char const *str);
+size_t    ft_strlcpy(char *dest, char const *src, size_t n);
+size_t    ft_strlcat(char *dest, const char *src, size_t n);
 
 // put-functions -> using write() needs a fd
 void			ft_putchar_fd(char c, int fd);
