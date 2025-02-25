51dbe79394fbd03a82fa38aa3e64d0bc15b8aace
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2025-02-25 23:18:15 +0100

    Update ft_strjoin.c

--- src/libft/ft_strjoin.c
+++ src/libft/ft_strjoin.c
@@ -15,29 +15,22 @@
 // Joins two string together
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int		i;
-	int		j;
+	int	i;
+	int	j;
 	char	*join;
+	int	len1 = (s1) ? ft_strlen(s1) : 0;
+	int	len2 = (s2) ? ft_strlen(s2) : 0;
 
-	join = (char *)malloc(sizeof(*s1) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	join = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
 	if (!join)
 		return (NULL);
 	i = 0;
 	j = 0;
-	while (s1[i] != '\0')
-	{
-		join[j] = s1[i];
-		j++;
-		i++;
-	}
+	while (s1 && s1[i] != '\0')
+		join[j++] = s1[i++];
 	i = 0;
-	while (s2[i] != '\0')
-	{
-		join[j] = s2[i];
-		j++;
-		i++;
-	}
+	while (s2 && s2[i] != '\0')
+		join[j++] = s2[i++];
 	join[j] = '\0';
 	return (join);
 }
-
